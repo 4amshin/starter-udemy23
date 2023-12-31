86aa215693d86d2e382629a2138b88f75a5308cb
4amshin <shinmendenz@gmail.com>
2023-12-31 10:16:54 +0800

    Praktik Yield & Section

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+
+@section('title', 'Homepage')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>HomePage</h1>
+        </div>
+
+        <div class="section-body">
+        </div>
+    </section>
+@endsection
