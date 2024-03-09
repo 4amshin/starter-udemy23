4122f0118f3d8a0091d4d3fbcdc6910dc77e134c
4amshin <shinmendenz@gmail.com>
2024-03-10 06:33:22 +0800

    Download File

--- app/Http/Controllers/DemoFileUploadController.php
+++ app/Http/Controllers/DemoFileUploadController.php
@@ -9,20 +9,32 @@ class DemoFileUploadController extends Controller
 {
     public function index()
     {
+        //menyimpan file ke disk local
         // Storage::put('new_file.txt', 'hello rely');
+
+        //menyimpan file ke disk public
         // Storage::disk('public')->put('public_file.txt', 'hello public');
+
+        //menyimpan file ke disk surat_tugas
         // Storage::disk('surat_tugas')->put('surat_tugas.txt', 'hello surat');
 
+        //menampilkan path dari setiap disk
         // $pathDefault = Storage::path('new_file.txt');
         // $pathPublic = Storage::disk('public')->path('public_file.txt');
         // $pathSuratTugas = Storage::disk('surat_tugas')->path('surat_tugas.txt');
-
         // ddd($pathDefault, $pathPublic, $pathSuratTugas);
 
-        $urlDefault = Storage::url('new_file.txt');
-        $urlPublic = Storage::disk('public')->url('public_file.txt');
-        $urlSuratTugas = Storage::disk('surat_tugas')->url('surat_tugas.txt');
+        //menampilkan url dari setiap disk
+        // $urlDefault = Storage::url('new_file.txt');
+        // $urlPublic = Storage::disk('public')->url('public_file.txt');
+        // $urlSuratTugas = Storage::disk('surat_tugas')->url('surat_tugas.txt');
+        // ddd($urlDefault, $urlPublic, $urlSuratTugas);
 
-        ddd($urlDefault, $urlPublic, $urlSuratTugas);
+        //menampilkan metadata(size, dll)
+        // $metaDefault = Storage::disk('local')->size('new_file.txt');
+        // ddd($metaDefault);
+
+        //mendownload file dari disk
+        return Storage::disk('surat_tugas')->download('surat_tugas.txt');
     }
 }
