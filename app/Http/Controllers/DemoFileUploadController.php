6ce6c9d5d908fd634cf4709bab41b27e5f902585
4amshin <shinmendenz@gmail.com>
2024-03-09 16:18:28 +0800

    Path & Url, and Permissions to display

--- app/Http/Controllers/DemoFileUploadController.php
+++ app/Http/Controllers/DemoFileUploadController.php
@@ -11,6 +11,18 @@ class DemoFileUploadController extends Controller
     {
         // Storage::put('new_file.txt', 'hello rely');
         // Storage::disk('public')->put('public_file.txt', 'hello public');
-        Storage::disk('surat_tugas')->put('surat_tugas.txt', 'hello surat');
+        // Storage::disk('surat_tugas')->put('surat_tugas.txt', 'hello surat');
+
+        // $pathDefault = Storage::path('new_file.txt');
+        // $pathPublic = Storage::disk('public')->path('public_file.txt');
+        // $pathSuratTugas = Storage::disk('surat_tugas')->path('surat_tugas.txt');
+
+        // ddd($pathDefault, $pathPublic, $pathSuratTugas);
+
+        $urlDefault = Storage::url('new_file.txt');
+        $urlPublic = Storage::disk('public')->url('public_file.txt');
+        $urlSuratTugas = Storage::disk('surat_tugas')->url('surat_tugas.txt');
+
+        ddd($urlDefault, $urlPublic, $urlSuratTugas);
     }
 }
